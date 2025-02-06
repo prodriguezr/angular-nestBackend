8be8901e4b2bb0b9d73a5d69171524611fa38011
Pablo Rodríguez <prodrigu75@gmail.com>
2025-02-06 11:12:45 -0300

    Connection string changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
+import { Logger } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
@@ -13,9 +13,10 @@ import { AuthModule } from './auth/auth.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
-        const mongoUri = `mongodb://${configService.get<string>('MONGO_USER')}:${configService.get<string>('MONGO_PASS')}@${configService.get<string>('MONGO_HOST')}:${configService.get<string>('MONGO_PORT')}/${configService.get<string>('MONGO_DB')}?authSource=${configService.get<string>('MONGO_AUTH_SOURCE')}`;
+        // const mongoUri = `mongodb://${configService.get<string>('MONGO_USER')}:${configService.get<string>('MONGO_PASS')}@${configService.get<string>('MONGO_HOST')}:${configService.get<string>('MONGO_PORT')}/${configService.get<string>('MONGO_DB')}?authSource=${configService.get<string>('MONGO_AUTH_SOURCE')}`;
+        const mongoUri = configService.get<string>('MONGO_URI') ?? '';
 
-        return { uri: mongoUri };
+        return { uri: mongoUri, dbName: configService.get<string>('MONGO_DB') };
       },
     }),
     AuthModule,
@@ -24,10 +25,12 @@ import { AuthModule } from './auth/auth.module';
   providers: [],
 })
 export class AppModule {
-  // private readonly logger = new Logger(AppModule.name);
-  // private readonly mongoUri: string;
-  // constructor(private readonly configService: ConfigService) {
-  //   this.mongoUri = `mongodb://${configService.get<string>('MONGO_USER')}:${configService.get<string>('MONGO_PASS')}@${configService.get<string>('MONGO_HOST')}:${configService.get<string>('MONGO_PORT')}/${configService.get<string>('MONGO_DB')}?authSource=${configService.get<string>('MONGO_AUTH_SOURCE')}`;
-  //   this.logger.log(this.mongoUri);
-  // }
+  private readonly logger = new Logger(AppModule.name);
+  private readonly mongoUri: string;
+  constructor(private readonly configService: ConfigService) {
+    // this.mongoUri = `mongodb://${configService.get<string>('MONGO_USER')}:${configService.get<string>('MONGO_PASS')}@${configService.get<string>('MONGO_HOST')}:${configService.get<string>('MONGO_PORT')}/${configService.get<string>('MONGO_DB')}?authSource=${configService.get<string>('MONGO_AUTH_SOURCE')}`;
+    // this.logger.log(this.mongoUri);
+    this.mongoUri = configService.get<string>('MONGO_URI') ?? '';
+    this.logger.log(this.mongoUri);
+  }
 }
